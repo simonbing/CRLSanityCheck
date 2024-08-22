ceddb6dbefe5faf9bcae49708a465f4c8cd8d80a
simonbing <bings@student.ethz.ch>
2024-08-22 15:55:12 +0200

    Adds bash script

---
+++ crc/cluster_scripts/contrast_crl_test_wrap.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+
+# Run jobs on GPU node
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+
+#SBATCH --account=bd1083
+#SBATCH --output=logs/slurm-%j.out
+#SBATCH --time=05:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=0
+
+# Activate environment
+source ~/.bashrc
+source activate crc
+
+# Loop over 5 random seeds
+for i in {1..5}
+do
+  SEED=$RANDOM
+  sbatch --wrap="python ../train.py --model contrast_crl --dataset lt_camera_v1 --experiment scm_2 \
+   --batch_size 512 --epochs 1 --seed ${SEED} --lat_dim 5 --run_name contrast_crl_no_changes_4k \
+   --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
+   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads"
+done
