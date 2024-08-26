867e739d5b90d9ca28070ea7b1d532f7499b6d09
simonbing <bings@student.ethz.ch>
2024-08-26 15:57:41 +0200

    Adds conrast_crl_synth_reprod submission script

--- crc/cluster_scripts/contrast_crl_synth_reprod.sh
+++ crc/cluster_scripts/contrast_crl_synth_reprod.sh
@@ -6,5 +6,5 @@ do
   SEED=$RANDOM
   sbatch cluster_gpu.sh python ../run_experiment.py --model contrast_crl --dataset contrast_synth --experiment synth_reprod \
    --batch_size 512 --epochs 200 --seed $SEED --lat_dim 5 --run_name contrast_crl_synth_reprod \
-   --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results
+   --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results  --metrics SHD,MCC
 done
