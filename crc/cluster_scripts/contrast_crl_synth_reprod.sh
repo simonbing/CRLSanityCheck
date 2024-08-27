12f1c9f09e0fef0f7259bebde0fe2eaf3f6cefec
simonbing <bings@student.ethz.ch>
2024-08-27 16:56:10 +0200

    Cluster script

--- crc/cluster_scripts/contrast_crl_synth_reprod.sh
+++ crc/cluster_scripts/contrast_crl_synth_reprod.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
 # Loop over 5 random seeds
-for SEED in 23871 725 31040 20988 454
+for i in {1..5}
 do
+  SEED=$RANDOM
   sbatch cluster_gpu.sh python ../run_experiment.py --model contrast_crl --dataset contrast_synth --experiment synth_reprod \
-   --batch_size 512 --epochs 200 --seed $SEED --lat_dim 5 --run_name contrast_crl_synth_reprod \
-   --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results  --metrics SHD,MCC \
-   --overwrite_data
+   --batch_size 512 --epochs 200 --seed $SEED --lat_dim 5 --run_name synth_reprod_correct_weights \
+   --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results  --metrics SHD,MCC
 done
