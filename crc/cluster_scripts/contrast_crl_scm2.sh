47f9ba39aa9aa58b4d500683106c7bd851ea53f9
simonbing <bings@student.ethz.ch>
2024-09-11 11:32:23 +0200

    Adds cluster script for scm2 10k experiment

--- crc/cluster_scripts/contrast_crl_scm2.sh
+++ crc/cluster_scripts/contrast_crl_scm2.sh
@@ -8,5 +8,6 @@ do
    --batch_size 512 --epochs 100 --seed $SEED --lat_dim 5 --run_name contrast_crl_10k_adam \
    --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
    --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
+   --overwrite_data \
    --metrics SHD,MCC
 done
