f8697eee5ab6f39ec8a6b6838ab86d2345e9aac7
simonbing <bings@student.ethz.ch>
2024-10-11 14:27:10 +0200

    PCL cluster script

---
+++ crc/cluster_scripts/pcl_no_whiten_lt_1.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+# Loop over 5 random seeds
+for i in {1..5}
+do
+  SEED=$RANDOM
+  sbatch cluster_gpu.sh python ../run_experiment.py --model pcl --dataset lt_camera_walks_v1 --task lt_pcl_1 \
+   --batch_size 512 --epochs 100 --seed $SEED --lat_dim 5 --run_name pcl_no_whiten_flat_lt \
+   --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
+   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
+   --overwrite_data \
+   --metrics MCC
+done
