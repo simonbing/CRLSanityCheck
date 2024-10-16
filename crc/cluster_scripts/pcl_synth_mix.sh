43e660cdb75d963046898ad7896ef951fb5ca99c
simonbing <bings@student.ethz.ch>
2024-10-16 14:42:36 +0200

    PCL synthetic mixinf of lt_1

--- crc/cluster_scripts/pcl_synth_mix.sh
+++ crc/cluster_scripts/pcl_synth_mix.sh
@@ -5,7 +5,7 @@ for i in {1..5}
 do
   SEED=$RANDOM
   sbatch cluster_gpu.sh python ../run_experiment.py --model pcl --dataset lt_camera_walks_v1_synth_mix --task lt_pcl_1 \
-   --batch_size 512 --epochs 1000 --seed $SEED --lat_dim 5 --run_name pcl_synth_mix_lt_1_300e \
+   --batch_size 512 --epochs 1000 --seed $SEED --lat_dim 5 --run_name pcl_synth_mix_lt_1_1ke \
    --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
    --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
    --overwrite_data \
