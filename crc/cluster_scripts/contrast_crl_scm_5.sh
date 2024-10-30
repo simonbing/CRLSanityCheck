706eba67361b51a0900f413aa5d09de8dc13f1f6
simonbing <bings@student.ethz.ch>
2024-10-30 11:07:34 +0100

    Cluster script

--- crc/cluster_scripts/contrast_crl_scm_5.sh
+++ crc/cluster_scripts/contrast_crl_scm_5.sh
@@ -4,8 +4,8 @@
 for i in {1..5}
 do
   SEED=$RANDOM
-  sbatch cluster_gpu.sh python ../run_experiment.py --model contrast_crl --dataset lt_camera_v1 --task lt_scm_5 \
-   --batch_size 512 --epochs 100 --seed $SEED --image_data --conv --lat_dim 3 --run_name contrast_crl_conv_scm_5_sgd \
+  sbatch cluster_gpu.sh python ../run_experiment.py --model contrast_crl_linear --dataset lt_camera_v1 --task lt_scm_5 \
+   --batch_size 512 --epochs 100 --seed $SEED --image_data --lat_dim 3 --run_name contrast_crl_lin_scm_5_sgd \
    --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
    --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
    --overwrite_data \
