b18fe3324f092b411109452af99cc7f3a043a7c4
simonbing <bings@student.ethz.ch>
2024-12-11 15:59:18 +0100

    Refactors base_method to unify train method for all child classes

--- crc/methods/base_method.py
+++ crc/methods/base_method.py
@@ -1,10 +1,16 @@
 """
 Abstract base class for CRL methods.
 """
-from abc import ABC, abstractmethod
-import torch
+import copy
 
-from crc.utils import get_device
+from abc import ABC, abstractmethod
+import numpy as np
+import torch
+from torch.utils.data import Subset, DataLoader
+from torch.nn.utils import clip_grad_norm_
+import wandb
+
+from crc.utils import get_device, train_val_test_split
 
 
 class CRLMethod(ABC):
@@ -40,8 +46,60 @@ class CRLMethod(ABC):
         """Must be called after defining self.model!"""
         return torch.optim.Adam(params=self.model.parameters(), lr=self.lr)
 
+    @abstractmethod
+    def train_step(self, data):
+        raise NotImplementedError
+
     def train(self):
+        train_idxs, val_idxs, test_idxs = train_val_test_split(
+            np.arange(len(self.dataset)),
+            train_size=self.train_size,
+            val_size=self.val_size,
+            random_state=self.seed)
 
+        train_dataset = Subset(self.dataset, train_idxs)
+        val_dataset = Subset(self.dataset, val_idxs)
 
+        train_dataloader = DataLoader(train_dataset, shuffle=True,
+                                      batch_size=self.batch_size)
+        val_dataloader = DataLoader(val_dataset, shuffle=False,
+                                    batch_size=self.batch_size)
 
-        pass
+        best_val_loss = np.inf
+        best_model = copy.deepcopy(self.model)
+
+        for epoch in range(self.epochs):
+            self.model.train()
+            # Training
+            for data in train_dataloader:
+                # Zero gradients
+                self.optimizer.zero_grad()
+
+                total_loss, loss_dict = self.train_step(data)  # dict
+
+                # Log losses
+                wandb.log({f'{key}_train': value for key, value in loss_dict.items()})
+
+                # Apply gradients
+                total_loss.backward()
+                # TODO: check if we always need gradient clipping
+                clip_grad_norm_(self.model.parameters(), max_norm=2.0,
+                                norm_type=2)
+                self.optimizer.step()
+
+            # Validation
+            if (epoch+1) % self.val_step == 0 or epoch == (self.epochs-1):
+                self.model.eval()
+                val_loss_values = []
+                with torch.no_grad():
+                    for data in val_dataloader:
+                        total_loss, loss_dict = self.train_step(data)
+                        val_loss_values.append(total_loss.item())
+                        # Log losses
+                        wandb.log({f'{key}_val': value for key, value in loss_dict.items()})
+
+                        if np.mean(val_loss_values) <= best_val_loss:
+                            best_val_loss = np.mean(val_loss_values)
+                            best_model = copy.deepcopy(self.model)
+
+        return best_model
