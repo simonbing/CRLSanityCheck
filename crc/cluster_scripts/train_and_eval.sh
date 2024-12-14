7ac2e56f97f2dab9c0a73fb86511ade6a5fd4f28
simonbing <bings@student.ethz.ch>
2024-12-14 16:26:50 +0100

    Cluster script

--- crc/cluster_scripts/train_and_eval.sh
+++ crc/cluster_scripts/train_and_eval.sh
@@ -8,6 +8,6 @@ do
   --dataset contrast_synthetic --task synth_reprod --encoder fc --epochs 200 --val_step 1 \
   --bs 512 --lr 0.0005 --lat_dim 5 --seed $SEED --run_name contrast_crl_synth_reprod \
   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
-  --output_root /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
+  --out_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
   --metrics mcc,shd
 done
