488e7359a52c591ea7cf4119cf43fd26467d7b9a
simonbing <bings@student.ethz.ch>
2024-12-16 10:32:32 +0100

    Cluster script

--- crc/cluster_scripts/train_and_eval.sh
+++ crc/cluster_scripts/train_and_eval.sh
@@ -4,7 +4,7 @@
 #for i in {1..5}
 for SEED in 30790 20966 27426 16345 23855
 do
-  SEED=$RANDOM
+#  SEED=$RANDOM
   sbatch cluster_gpu.sh python ../apps/train_and_evaluate_method.py --method contrast_crl \
   --dataset contrast_synthetic --task synth_reprod --encoder fc --epochs 200 --val_step 1 \
   --bs 512 --lr 0.0005 --lat_dim 5 --seed $SEED --run_name contrast_crl_synth_reprod \
