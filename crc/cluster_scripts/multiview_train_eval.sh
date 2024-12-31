b4d92d73a87228cb0078920c7ac45ec1aac9f3b4
simonbing <bings@student.ethz.ch>
2024-12-31 16:14:44 +0100

    Cluster script

--- crc/cluster_scripts/multiview_train_eval.sh
+++ crc/cluster_scripts/multiview_train_eval.sh
@@ -3,7 +3,7 @@
 # Loop over 5 random seeds
 for i in {1..5}
 do
-#  SEED=$RANDOM
+  SEED=$RANDOM
   sbatch cluster_gpu.sh python ../apps/train_and_evaluate_method.py --method multiview \
   --dataset lt_camera_v1 --task lt_scm_2 --epochs 200 --val_step 1 \
   --bs 256 --lr 0.0001 --lat_dim 5 --seed $SEED --run_name multiview_test_0 \
