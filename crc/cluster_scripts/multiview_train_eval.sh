a500ffb4d441f11d7244d2c0b6d77b6a95399178
simonbing <bings@student.ethz.ch>
2024-12-31 16:05:39 +0100

    Cluster script

---
+++ crc/cluster_scripts/multiview_train_eval.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+# Loop over 5 random seeds
+for i in {1..5}
+do
+#  SEED=$RANDOM
+  sbatch cluster_gpu.sh python ../apps/train_and_evaluate_method.py --method multiview \
+  --dataset lt_camera_v1 --task lt_scm_2 --epochs 200 --val_step 1 \
+  --bs 256 --lr 0.0001 --lat_dim 5 --seed $SEED --run_name multiview_test_0 \
+  --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
+  --out_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
+  --metrics r2
+done
