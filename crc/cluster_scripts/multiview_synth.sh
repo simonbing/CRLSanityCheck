ec8aaecad96df167bcf926ca8134f1d88afc30ee
simonbing <bings@student.ethz.ch>
2025-01-07 15:28:44 +0100

    debug cluster script

--- crc/cluster_scripts/multiview_synth.sh
+++ crc/cluster_scripts/multiview_synth.sh
@@ -4,7 +4,7 @@
 for i in {1..5}
 do
   SEED=$RANDOM
-  sbatch cluster_gpu.sh python ../apps/train_and_evaluate_method.py --method multiview \
+  sbatch cluster_gpu.sh CUDA_LAUNCH_BLOCKING=1 python ../apps/train_and_evaluate_method.py --method multiview \
   --dataset multiview_synthetic --task lt_scm_2 --epochs 500 --val_step 10 \
   --bs 4096 --lr 0.0001 --lat_dim 5 --seed $SEED --run_name multiview_synth_reprod_0 \
   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
