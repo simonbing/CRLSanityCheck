eaa1ad4ea01a6762631665badd53aec6d8540897
simonbing <bings@student.ethz.ch>
2025-01-10 14:24:40 +0100

    Lower lr

--- crc/methods/base_method.py
+++ crc/methods/base_method.py
@@ -62,10 +62,10 @@ class CRLMethod(ABC):
 
         train_dataloader = DataLoader(train_dataset, shuffle=True,
                                       batch_size=self.batch_size,
-                                      drop_last=False)
+                                      drop_last=True)
         val_dataloader = DataLoader(val_dataset, shuffle=False,
                                     batch_size=self.batch_size,
-                                    drop_last=False)
+                                    drop_last=True)
 
         best_val_loss = np.inf
         best_model = copy.deepcopy(self.model)
