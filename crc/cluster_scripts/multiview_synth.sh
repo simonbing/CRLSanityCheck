a908f72079b2d7d396ea0fbbfe6a7a77493f1d58
simonbing <bings@student.ethz.ch>
2025-01-10 15:27:48 +0100

    Lower lr

--- crc/cluster_scripts/multiview_synth.sh
+++ crc/cluster_scripts/multiview_synth.sh
@@ -6,7 +6,7 @@ do
 SEED=$RANDOM
   sbatch cluster_gpu.sh python ../apps/train_and_evaluate_method.py --method multiview \
   --dataset multiview_synthetic_chambers_indep --task lt_scm_2 --epochs 500 --val_step 10 \
-  --bs 256 --lr 0.00001 --lat_dim 5 --seed $SEED --run_name multiview_synth_chambers_indep_2\
+  --bs 4096 --lr 0.00001 --lat_dim 5 --seed $SEED --run_name multiview_synth_chambers_indep_2\
   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
   --out_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results \
   --metrics r2
