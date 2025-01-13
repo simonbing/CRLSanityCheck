a5c85881ce3180709cea791fcb7557b5a0e900d0
simonbing <bings@student.ethz.ch>
2025-01-13 17:47:08 +0100

    debug

--- crc/cluster_scripts/contrastive_crl_synth_reprod.sh
+++ crc/cluster_scripts/contrastive_crl_synth_reprod.sh
@@ -3,7 +3,7 @@
 # Loop over 5 random seeds
 #for i in {1..5}
 #do
-SEED=$RANDOM
+SEED=26705
 sbatch cluster_gpu.sh python ../contrastive_crl_experiment.py --model contrast_crl --dataset contrast_synth --task synth_reprod \
  --batch_size 512 --epochs 100 --seed $SEED --lat_dim 5 --optim adam --run_name synth_reprod_refactor_adam_0 \
  --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
