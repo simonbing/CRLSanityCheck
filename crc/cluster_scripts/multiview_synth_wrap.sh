000155411f4d9aed2a7931fa507b6fc5739fa631
simonbing <bings@student.ethz.ch>
2025-01-16 19:22:48 +0100

    Cluster script

--- crc/cluster_scripts/multiview_synth_wrap.sh
+++ crc/cluster_scripts/multiview_synth_wrap.sh
@@ -4,7 +4,7 @@
 for i in {1..3}
 do
 SEED=$RANDOM
-  sbatch cluster_gpu.sh python ../multiview_experiment.py --method multiview \
+  sbatch cluster_gpu.sh python ../multiview_experiment.py --model multiview \
   --dataset multiview_synth --task synth_re --train_steps 300000 \
   --batch_size 128 --lr 0.0001 --lat_dim 4 --seed $SEED --run_name multiview_synth_wrap_0 \
   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
