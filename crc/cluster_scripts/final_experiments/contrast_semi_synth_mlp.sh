307fd0bb642428d0ab3970d54bcd2d620a0cd048
simonbing <bings@student.ethz.ch>
2025-01-18 11:36:38 +0100

    Cluster script

--- crc/cluster_scripts/final_experiments/contrast_semi_synth_mlp.sh
+++ crc/cluster_scripts/final_experiments/contrast_semi_synth_mlp.sh
@@ -4,7 +4,7 @@
 for i in {1..3}
 do
   SEED=$RANDOM
-  sbatch cluster_gpu.sh python ../contrastive_crl_experiment.py --model contrast_crl --dataset contrast_semi_synth_mlp --task lt_scm_2 \
+  sbatch ../cluster_gpu.sh python ../contrastive_crl_experiment.py --model contrast_crl --dataset contrast_semi_synth_mlp --task lt_scm_2 \
    --batch_size 512 --epochs 100 --seed $SEED --lat_dim 5 --optim adam --run_name contrast_crl_semi_synth_mlp_final_0 \
    --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/contrast_crl_latents \
    --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
