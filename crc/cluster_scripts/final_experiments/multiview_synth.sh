33a202db2067476e44bfa2558161a01f5a630c3e
simonbing <bings@student.ethz.ch>
2025-01-21 16:15:47 +0100

    Cluster script

--- crc/cluster_scripts/final_experiments/multiview_synth.sh
+++ crc/cluster_scripts/final_experiments/multiview_synth.sh
@@ -5,7 +5,7 @@ for i in {1..2}
 #for SEED in 30325 9813 11470
 do
 SEED=$RANDOM
-  sbatch cluster_gpu.sh python ../multiview_experiment.py --model multiview_crl \
+  sbatch ../cluster_gpu.sh python ../../multiview_experiment.py --model multiview_crl \
   --dataset multiview_synth --task synth_re_mix_2 --train_steps 300000 \
   --batch_size 128 --lr 0.0001 --lat_dim 4 --seed $SEED --run_name multiview_synth_0 \
   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
