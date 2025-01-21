26bf420fb442b4859613e49cdc1320eb5a026f25
simonbing <bings@student.ethz.ch>
2025-01-21 17:59:17 +0100

    Cluster script debug

--- crc/cluster_scripts/final_experiments/multiview_semi_synth_decoder.sh
+++ crc/cluster_scripts/final_experiments/multiview_semi_synth_decoder.sh
@@ -6,7 +6,7 @@
 SEED=$RANDOM
 sbatch ../cluster_gpu.sh python ../../multiview_experiment.py --model multiview_crl \
 --dataset chambers_semi_synth_decoder --task chambers_semi_synth --exp_name scm_2 --train_steps 300000 \
---batch_size 128 --lr 0.0001 --lat_dim 5 --seed $SEED --run_name multiview_semi_synth_dec_debug \
+--batch_size 256 --lr 0.0001 --lat_dim 5 --seed $SEED --run_name multiview_semi_synth_dec_debug \
 --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/contrast_crl_latents \
 --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
 #done
