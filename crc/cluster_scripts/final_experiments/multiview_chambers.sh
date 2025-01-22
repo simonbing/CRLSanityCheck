994a8c8be8f0d8f2b005d062a653ab7574887acf
simonbing <bings@student.ethz.ch>
2025-01-22 17:41:19 +0100

    Cluster script

--- crc/cluster_scripts/final_experiments/multiview_chambers.sh
+++ crc/cluster_scripts/final_experiments/multiview_chambers.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
 
 # Loop over 5 random seeds
-#for i in {1..5}
-#do
-SEED=$RANDOM
-sbatch ../cluster_gpu.sh python ../../multiview_experiment.py --model multiview_crl \
---dataset lt_crl_benchmark_v1 --task contrast_crl_real --exp_name buchholz_1 --train_steps 30000 \
---batch_size 512 --lr 0.0001 --lat_dim 5 --seed $SEED --run_name multiview_chambers_0 \
---data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
---root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
-#done
+for i in {1..2}
+do
+  SEED=$RANDOM
+  sbatch ../cluster_gpu.sh python ../../multiview_experiment.py --model multiview_crl \
+  --dataset lt_crl_benchmark_v1 --task contrast_crl_real --exp_name buchholz_1 --train_steps 30000 \
+  --batch_size 512 --lr 0.0001 --lat_dim 5 --seed $SEED --run_name multiview_chambers_0 \
+  --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
+  --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
+done
