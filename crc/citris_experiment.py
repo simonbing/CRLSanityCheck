d5b743290b265c678f2c79f1945e8666128bdb71
simonbing <bings@student.ethz.ch>
2025-01-22 16:38:44 +0100

    Citrus cluster script semi synth

--- crc/citris_experiment.py
+++ crc/citris_experiment.py
@@ -129,7 +129,7 @@ def main(argv):
                 root_dir=train_dir,
                 max_epochs=FLAGS.epochs,
                 logger_name=f'{FLAGS.model}_{FLAGS.lat_dim}l_{model_args["num_causal_vars"]}b_{FLAGS.c_hid}hid_{data_name}',
-                check_val_every_n_epoch=2 if not gettrace() else 2, #25
+                check_val_every_n_epoch=5 if not gettrace() else 2, #25
                 progress_bar_refresh_rate=1 if not gettrace() else 1, #0
                 callback_kwargs={'dataset': datasets['train'],
                                  'correlation_dataset': datasets['val'],  # Independent latents here
