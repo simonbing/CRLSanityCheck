4412e154cea1e53b32329152d640a0b1b342f94e
simonbing <bings@student.ethz.ch>
2025-01-22 18:08:07 +0100

    Cluster script

--- crc/cluster_scripts/final_experiments/contrast_semi_synth_decoder.sh
+++ crc/cluster_scripts/final_experiments/contrast_semi_synth_decoder.sh
@@ -4,7 +4,7 @@
 #for i in {1..3}
 #do
 SEED=$RANDOM
-sbatch ../cluster_gpu.sh python ../../contrastive_crl_experiment.py --model contrast_crl --dataset contrast_semi_synth_decoder --task lt_scm_2 \
+sbatch ../cluster_gpu.sh python ../../contrastive_crl_experiment.py --model contrast_crl --dataset contrast_semi_synth_decoder --task contrast_crl_real \
  --batch_size 512 --epochs 100 --seed $SEED --lat_dim 5 --optim adam --run_name contrast_crl_semi_synth_sanity_1 \
  --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads \
  --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
