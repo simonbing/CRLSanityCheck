55fd1c708baa189287acf7ac0d6be9a9445e7db1
simonbing <bings@student.ethz.ch>
2025-01-23 09:39:24 +0100

    Cluster script

--- crc/cluster_scripts/final_experiments/citris_semi_synth_decoder.sh
+++ crc/cluster_scripts/final_experiments/citris_semi_synth_decoder.sh
@@ -5,7 +5,7 @@ for i in {1..3}
 do
   SEED=$RANDOM
   sbatch ../cluster_gpu.sh python ../../citris_experiment.py --model citrisvae --dataset chambers_semi_synth_decoder --task semi_synth_dec \
-   --batch_size 512 --epochs 100 --seed $SEED --lat_dim 16 --run_name citris_semi_synth_dec_1 \
+   --batch_size 512 --epochs 50 --seed $SEED --lat_dim 16 --run_name citris_semi_synth_dec_2 \
    --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/citris_latents \
    --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
 done
