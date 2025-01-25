4adfd51b8ff5fb5bebeb16bdd70f6e7bfd3b1699
simonbing <bings@student.ethz.ch>
2025-01-25 11:09:33 +0100

    cluster debug

--- crc/cluster_scripts/final_experiments/citris_semi_synth_decoder.sh
+++ crc/cluster_scripts/final_experiments/citris_semi_synth_decoder.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/bash
 
 # Loop over 3 random seeds
-for i in {1..3}
-do
-  SEED=$RANDOM
-  sbatch ../cluster_gpu.sh python ../../citris_experiment.py --model citrisvae --dataset chambers_semi_synth_decoder --task semi_synth_dec \
-   --batch_size 512 --epochs 30 --seed $SEED --lat_dim 5 --run_name citris_semi_synth_dec_auto_d_5 \
-   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/citris_latents \
-   --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
-done
+#for i in {1..3}
+#do
+SEED=$RANDOM
+sbatch ../cluster_gpu.sh python ../../citris_experiment.py --model citrisvae --dataset chambers_semi_synth_decoder --task semi_synth_dec \
+ --batch_size 512 --epochs 2 --seed $SEED --lat_dim 5 --run_name citris_semi_synth_debug_o \
+ --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/citris_latents \
+ --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
+#done
