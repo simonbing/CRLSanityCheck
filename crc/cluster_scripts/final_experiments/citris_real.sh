27f895afdda61de9ee93ef80b6dd61f29e3ea05c
simonbing <bings@student.ethz.ch>
2025-01-25 20:11:40 +0100

    Adds support for real data to citris

---
+++ crc/cluster_scripts/final_experiments/citris_real.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+# Loop over 3 random seeds
+#for i in {1..3}
+#do
+SEED=$RANDOM
+sbatch ../cluster_gpu.sh python ../../citris_experiment.py --model citrisvae --dataset chambers --task real\
+ --batch_size 512 --epochs 30 --seed $SEED --lat_dim 16 --run_name citris_real_0 \
+ --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads_citris \
+ --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
+#done
