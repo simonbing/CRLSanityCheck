85dfe31444edce20bcfdeefa130c3e12a1b50221
simonbing <bings@student.ethz.ch>
2025-01-26 18:02:55 +0100

    Cluster script

--- crc/cluster_scripts/final_experiments/multiview_semi_synth_decoder.sh
+++ crc/cluster_scripts/final_experiments/multiview_semi_synth_decoder.sh
@@ -5,7 +5,7 @@ for i in {1..5}
 do
   SEED=$RANDOM
   sbatch ../cluster_gpu.sh python ../../multiview_experiment.py --model multiview_crl \
-  --dataset chambers_semi_synth_decoder --task chambers_semi_synth_add_sens --exp_name buchholz_1_synth_det --train_steps 28500 \
+  --dataset chambers_semi_synth_decoder --task chambers_semi_synth_less_sens --exp_name buchholz_1_synth_det --train_steps 28500 \
   --batch_size 512 --lr 0.0001 --lat_dim 5 --seed $SEED --run_name multiview_semi_synth_dec_less_sensors_0 \
   --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads_citris \
   --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
