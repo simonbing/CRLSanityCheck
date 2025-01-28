dc6e7b7e755fcf032acaac3b39a13af81005f613
simonbing <bings@student.ethz.ch>
2025-01-28 14:36:40 +0100

    cluster script

--- crc/cluster_scripts/final_experiments/citris_real.sh
+++ crc/cluster_scripts/final_experiments/citris_real.sh
@@ -5,7 +5,7 @@ for i in {1..5}
 do
   SEED=$RANDOM
   sbatch ../cluster_gpu.sh python ../../citris_experiment.py --model citrisvae --dataset chambers --task real\
-   --batch_size 512 --epochs 250 --seed $SEED --lat_dim 16 --run_name citris_real_d16_full_0 \
+   --batch_size 512 --epochs 600 --seed $SEED --lat_dim 16 --run_name citris_real_d16_full_600_0 \
    --data_root /work/bd1083/b382081/projects/CausalRepresentationChambers/data/chamber_downloads_citris \
    --root_dir /work/bd1083/b382081/projects/CausalRepresentationChambers/results
 done
